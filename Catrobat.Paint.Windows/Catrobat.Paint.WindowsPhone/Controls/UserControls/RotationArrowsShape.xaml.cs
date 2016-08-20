2b3a89216cf8b3c17d88b7b407f7e4f1c121f091
Harald Schaffernak <haraldschaffernak@yahoo.de>
2016-08-20 11:56:34 +0200

    Paint exclusive repo!

---
+++ Catrobat.Paint.Windows/Catrobat.Paint.WindowsPhone/Controls/UserControls/RotationArrowsShape.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Catrobat.Paint.WindowsPhone.Controls.UserControls
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class RotationArrowsShape
+    {
+        public RotationArrowsShape()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
